bb14b5bdf91dc1d714788a2ccc39c20198b7bb18
guoqunfei <50896478+guoqunfei@users.noreply.github.com>
2022-01-26 18:17:37 +0800

    Create ortholog_identification.sh

---
+++ Ortholog_identification/ortholog_identification.sh
@@ -0,0 +1,4 @@
+# Take the ortholog identification of pharaoh ants and honeybees as an example.
+## First of all, download all the data used in this analysis.
+## Aeml.gtf.gz : https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/254/395/GCF_003254395.2_Amel_HAv3.1/GCF_003254395.2_Amel_HAv3.1_genomic.gtf.gz
+## Amel
